4a1b4279b174d9067f25b854600d2408b9bbb2b9
desiree <desireecamillevieira@gmail.com>
2024-06-20 16:37:23 +0200

    sessions id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Head from "next/head";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import { useKeenSlider } from "keen-slider/react";
@@ -27,25 +28,32 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false} >
-            <Product
-              key={product.id}
-              className="keen-slider__slide"
-            >
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        );
-      })}
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product key={product.id} className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
   );
 }
 
